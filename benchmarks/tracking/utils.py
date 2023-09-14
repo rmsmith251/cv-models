81d87779e6e89e8202dc2314157d11e38c720935
rmsmith251 <ryanmsmith251@gmail.com>
2023-09-13 17:09:50 -0700

    add shapely comparison benchmark

--- benchmarks/tracking/utils.py
+++ benchmarks/tracking/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+from shapely.geometry import Polygon
 
 from vision.tracking.utils import vectorized_bbox_iou
 
@@ -34,3 +35,32 @@ def test_benchmark_bbox_iou(benchmark, arr1_mult, arr2_mult):
     arr2 = np.asarray([[90, 160, 190, 260]] * arr2_mult)
 
     benchmark(vectorized_bbox_iou, arr1, arr2)
+
+
+def bbox_to_shapely(arr):
+    return ((arr[0], arr[1]), (arr[0], arr[3]), (arr[2], arr[3]), (arr[2], arr[1]))
+
+
+def shapely_iou(poly1: Polygon, polys2: list[Polygon]) -> np.ndarray:
+    intersections = [poly1.intersection(poly).area for poly in polys2]
+    unions = [
+        poly1.area + poly.area - intersections[idx] for idx, poly in enumerate(polys2)
+    ]
+    return np.array(intersections) / (np.array(unions) + 1e-8)
+
+
+@pytest.mark.parametrize("arr1_mult,arr2_mult", test_runs[:10])
+def test_shapely_bbox_iou(benchmark, arr1_mult, arr2_mult):
+    """
+    This only serves as an example for how much faster the vectorized version is. This converts the
+    bounding boxes into Shapely Polygons and then computes the IoU for each polygon against the other
+    array.
+    """
+    arr1 = np.asarray([[100, 150, 200, 250]] * arr1_mult)
+    arr2 = np.asarray([[90, 160, 190, 260]] * arr2_mult)
+
+    @benchmark
+    def run():
+        polys_1 = [Polygon(bbox_to_shapely(arr)) for arr in arr1]
+        polys_2 = [Polygon(bbox_to_shapely(arr)) for arr in arr2]
+        _ = [shapely_iou(poly, polys_2) for poly in polys_1]
