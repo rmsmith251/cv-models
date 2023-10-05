2d558478b0549490ac793b804af9718abc9d2b67
Ryan Smith <ryanmsmith251@gmail.com>
2023-10-05 16:30:56 -0700

    add pretrained torchvision mask-rcnn models and fix base inference model

--- vision/models/base.py
+++ vision/models/base.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from typing import List, Union
+from typing import Callable, List, Union
 
 import cv2
 import numpy as np
@@ -9,9 +9,13 @@ import torch
 import torch.nn as nn
 from PIL import Image
 
+from vision.utils import ImageConverter
+
 IMAGE_TYPES = Union[
     np.ndarray, List[np.ndarray], List[Image.Image], torch.Tensor, List[torch.Tensor]
 ]
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 class BaseTrainer(nn.Module):
@@ -19,42 +23,35 @@ class BaseTrainer(nn.Module):
         super().__init__()
 
 
-class BaseInferenceModel(nn.Module):
-    def __init__(self, max_image_size: int = 800, device: str | torch.device = "cpu"):
+class BaseImageInferenceModel(nn.Module):
+    def __init__(
+        self,
+        model_name: str,
+        max_image_size: int = 800,
+        device: str | torch.device | None = DEVICE,
+    ):
         super().__init__()
+        self.model_name = model_name
+        self.max_image_size = max_image_size
         self.device = device
-
-    def preprocess_pil_images(self, inputs: IMAGE_TYPES) -> torch.Tensor:
-        pass
-
-    def preprocess_numpy_array(self, inputs: IMAGE_TYPES) -> torch.Tensor:
-        torch.Tensor(cv2.cvtColor(inputs, cv2.COLOR_BGR2RGB))
-
-    def preprocess_tensors(self, inputs: IMAGE_TYPES) -> torch.Tensor:
-        if isinstance(inputs, list):
-            out = torch.Tensor(inputs)
-        else:
-            out = inputs
-        return out
+        self.model: Callable | None = None
 
     def preprocess(self, inputs: IMAGE_TYPES) -> torch.Tensor:
-        if isinstance(inputs, list):
-            sample = inputs[0]
-        else:
-            sample = inputs
+        tensors = ImageConverter.from_any(inputs).to_tensor(normalize=True)
+        return tensors.to(self.device)
 
-        if isinstance(sample, np.ndarray):
-            return self.preprocess_numpy_array(inputs)
-        elif isinstance(sample, Image.Image):
-            return self.preprocess_pil_images(inputs)
-        elif isinstance(sample, torch.Tensor):
-            return self.preprocess_tensors(inputs)
-        else:
-            raise ValueError(f"Object of type {type(sample)} is not supported")
-
-    @abstractmethod
     def predict(self, inputs: torch.Tensor) -> torch.Tensor:
-        ...
+        if self.model is None:
+            raise NotImplementedError(f"{self.model_name} is not implemented")
+
+        images = self.preprocess(inputs).to(dtype=torch.float16)
+        with torch.no_grad(), torch.autocast(
+            device_type=self.device, dtype=torch.float16
+        ):
+            out = self.model(images)
+            if len(out) == 1:
+                return out[0]
+            return out
 
     def __call__(self, inputs: IMAGE_TYPES) -> torch.Tensor:
-        return self.predict(self.preprocess(inputs))
+        return self.predict(inputs)
--- vision/models/rcnn/mask_rcnn.py
+++ vision/models/rcnn/mask_rcnn.py
@@ -1,38 +1,47 @@
 from __future__ import annotations
 
 import torch
-from torch import Tensor
+import torchvision
 from torchvision.models import detection
 
-from vision.models.base import IMAGE_TYPES, BaseInferenceModel
+from vision.models.base import BaseImageInferenceModel
 
 TORCHVISION_ALIASES = {
     "mask-rcnn": detection.maskrcnn_resnet50_fpn,
     "mask-rcnn-v2": detection.maskrcnn_resnet50_fpn_v2,
 }
 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-class MaskRCNN(BaseInferenceModel):
+
+class MaskRCNN(BaseImageInferenceModel):
     """
     This is just a wrapper around the Torchvision models for now until I implement Mask-RCNN for different
     backbones.
     """
 
-    def __init__(self, model_name: str = "mask-rcnn", max_image_size: int = 800):
-        super().__init__()
+    def __init__(
+        self,
+        model_name: str = "mask-rcnn",
+        max_image_size: int = 800,
+        weights: str = "DEFAULT",
+        device: torch.device | str | None = None,
+    ):
+        super().__init__(
+            model_name=model_name, max_image_size=max_image_size, device=device
+        )
         self.model_name = model_name
+        self.weights = weights
+        self.device = device if device is not None else DEVICE
 
         assert (
             self.model_name in TORCHVISION_ALIASES
         ), f"{self.model_name} is not a valid model name"
-        self.model = TORCHVISION_ALIASES[self.model_name]
+        self.model = TORCHVISION_ALIASES[self.model_name](weights=self.weights).to(
+            self.device
+        )
+        self.model.eval()
 
     @classmethod
     def aliases() -> list[str]:
         return list(TORCHVISION_ALIASES.keys())
-
-    def preprocess(self, inputs: IMAGE_TYPES) -> torch.Tensor:
-        pass
-
-    def predict(self, inputs: Tensor) -> Tensor:
-        pass
